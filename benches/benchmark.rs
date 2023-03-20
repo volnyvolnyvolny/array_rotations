1d1a61781546c52a6d91225f5a1a24e08ab9bc13
Valentin <volnyvolnyvolny@ya.ru>
2023-03-20 18:47:41 +0300

    Bench update.

--- benches/benchmark.rs
+++ benches/benchmark.rs
@@ -43,6 +43,44 @@ fn test(rotate: unsafe fn(left: usize, mid: *mut usize, right: usize), left: usi
     // }
 }
 
+// fn test_swap<T>(swap: unsafe fn(x: *mut T, y: *mut T, count: usize), x: *mut T, y: *mut T, count: usize){
+// //    if left <= right {
+        // unsafe{ swap(x, y, count) }
+        // // unsafe{ rotate(right, p.add(right - left), left) }
+    // // } else {
+        // // unsafe{ rotate(left, p, right) }
+        // // unsafe{ rotate(right, p.sub(left - right), left) }
+    // // }
+// }
+// 
+// unsafe fn juggler_swap<T>(x: *mut T, _y: *mut T, count: usize) {
+    // ptr_juggling_rotate(count, x.add(count), count);
+// }
+// 
+// fn case_swap_nonoverlapping(c: &mut Criterion, ls: &[usize]) {
+    // let mut group = c.benchmark_group(format!("Nonoverlapping swaps").as_str());
+    // let mut v = seq(*ls.into_iter().max().unwrap());
+// 
+    // for l in ls {
+        // let x =
+            // unsafe {
+                // let x = &v[..].as_mut_ptr();
+                // x.clone()
+            // };
+// 
+        // let y = unsafe{ x.add(l / 2) };
+// 
+        // group.bench_with_input(BenchmarkId::new("Default", l), l, |b, l| b.iter(||
+            // test_swap(ptr::swap_nonoverlapping::<usize>, x, y, l / 2)
+        // ));
+        // group.bench_with_input(BenchmarkId::new("Juggler", l), l, |b, l| b.iter(||
+            // test_swap(juggler_swap::<usize>, x, y, l / 2)
+        // ));
+    // }
+// 
+    // group.finish();
+// }
+
 fn case_grail_vs_drill_vs_helix_vs_piston_vs_bridge(c: &mut Criterion, len: usize, ls: &[usize]) {
     let mut group = c.benchmark_group(format!("Rotations (GM vs Grail vs Drill vs Helix vs Piston vs Bridge) (len = {len})").as_str());
     let mut v = seq(len);
@@ -63,6 +101,42 @@ fn case_grail_vs_drill_vs_helix_vs_piston_vs_bridge(c: &mut Criterion, len: usiz
         group.bench_with_input(BenchmarkId::new("GM", l),       l, |b, l| b.iter(|| test(ptr_griesmills_rotate::<usize>, l.clone(), p, r)));
 
         group.bench_with_input(BenchmarkId::new("Bridge", l),   l, |b, l| b.iter(|| test(ptr_bridge_rotate::<usize>,     l.clone(), p, r)));
+    }
+
+    group.finish();
+}
+
+fn case_all(c: &mut Criterion, len: usize, ls: &[usize]) {
+    let mut group = c.benchmark_group(format!("All rotations (len = {len})").as_str());
+    let mut v = seq(len);
+
+    for l in ls {
+        let p =
+            unsafe {
+                let p = &v[..].as_mut_ptr().add(l.clone());
+                p.clone()
+            };
+    
+        let r = len - l;
+
+        group.bench_with_input(BenchmarkId::new("Aux", l),      l, |b, l| b.iter(|| test(ptr_aux_rotate::<usize>,        l.clone(), p, r)));
+        group.bench_with_input(BenchmarkId::new("Bridge", l),   l, |b, l| b.iter(|| test(ptr_bridge_rotate::<usize>,     l.clone(), p, r)));
+
+        group.bench_with_input(BenchmarkId::new("Default", l),  l, |b, l| b.iter(|| test(stable_ptr_rotate::<usize>,     l.clone(), p, r)));
+
+        group.bench_with_input(BenchmarkId::new("Piston", l),   l, |b, l| b.iter(|| test(ptr_piston_rotate::<usize>,     l.clone(), p, r)));
+        group.bench_with_input(BenchmarkId::new("Helix", l),    l, |b, l| b.iter(|| test(ptr_helix_rotate::<usize> ,     l.clone(), p, r)));
+        group.bench_with_input(BenchmarkId::new("Grail", l),    l, |b, l| b.iter(|| test(ptr_grail_rotate::<usize>,      l.clone(), p, r)));
+        group.bench_with_input(BenchmarkId::new("Drill", l),    l, |b, l| b.iter(|| test(ptr_drill_rotate::<usize>,      l.clone(), p, r)));
+
+        group.bench_with_input(BenchmarkId::new("GM", l),       l, |b, l| b.iter(|| test(ptr_griesmills_rotate::<usize>, l.clone(), p, r)));
+ 
+        group.bench_with_input(BenchmarkId::new("Rev", l),      l, |b, l| b.iter(|| test(ptr_reversal_rotate::<usize>,   l.clone(), p, r)));
+        group.bench_with_input(BenchmarkId::new("Contrev", l),  l, |b, l| b.iter(|| test(ptr_contrev_rotate::<usize>,    l.clone(), p, r)));
+
+        group.bench_with_input(BenchmarkId::new("Trinity", l),  l, |b, l| b.iter(|| test(ptr_trinity_rotate::<usize>,    l.clone(), p, r)));
+
+        group.bench_with_input(BenchmarkId::new("Juggling", l), l, |b, l| b.iter(|| test(ptr_juggling_rotate::<usize>,   l.clone(), p, r)));
     }
 
     group.finish();
@@ -226,6 +300,13 @@ fn case_50000_stable_vs_contrev_vs_bridge(c: &mut Criterion, len: usize, ls: &[u
 }
 
 fn benchmark(c: &mut Criterion) {
+    case_all(c,   10, &[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+    case_all(c,  100, &[0, 1, 16, 32, 33, 40, 50, 60, 67, 68, 100]);
+    case_all(c, 1000, &[0, 32, 33, 100, 200, 300, 400, 500, 600, 700, 800, 900, 967, 968, 1000]);
+    // case_all(c, 10000, &[1, 16, 32, 33, 40, 50, 60, 68, 69, 84, 100]);
+    // case_all(c, 100000, &[1, 16, 32, 33, 40, 50, 60, 68, 69, 84, 100]);
+    // case_all(c, 1000000, &[1, 16, 32, 33, 40, 50, 60, 68, 69, 84, 100]);
+
 //    case_stable(c, 100, &[1, 16, 32, 33, 40, 50, 60, 68, 69, 84, 100]);
 
 //    case_stable(c, 1000000, &[1000, 100000, 200000, 300000, 400000, 499990]);
@@ -236,7 +317,7 @@ fn benchmark(c: &mut Criterion) {
 
 //    case_grail_vs_drill_vs_helix_vs_piston_vs_bridge(c, 100000, &[33, 10000, 20000, 30000, 40000, 49983, 49992, 50008, 50017, 60000, 70000, 80000, 90000, 99967]);
 
-    case_swap_nonoverlapping(c, &[33, 10000, 20000, 30000, 40000, 49983, 49992, 50008, 50017, 60000, 70000, 80000, 90000, 99967]);
+//    case_swap_nonoverlapping(c, &[33, 10000, 20000, 30000, 40000, 49983, 49992, 50008, 50017, 60000, 70000, 80000, 90000, 99967]);
 
 //    case_contrev_vs_trinity(c, 100000, &[33, 10000, 20000, 30000, 40000, 49983, 49992, 50008, 50017, 60000, 70000, 80000, 90000, 99967]);
 
