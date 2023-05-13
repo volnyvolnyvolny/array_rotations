88c5946ec8d7d60a00e6945d7df33d4bcb9d6686
Valentin Vasilev <volnyvolnyvolny@ya.ru>
2023-05-13 21:15:32 +0300

    PACKAGE VERSION bump, renaming + unicode-perl feature

--- benches/rotations.rs
+++ benches/rotations.rs
@@ -1,5 +1,5 @@
 use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
-use rust_rotates::*;
+use rust_rotations::*;
 // use pprof::criterion::{Output, PProfProfiler};
 
 // use std::time::Duration;
